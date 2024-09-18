e28b7cabbd6c62f06a55684a098bf58b01d91257
OSN <luckyartoolkit@gmail.com>
2024-09-18 23:45:08 +0800

    調整 loading

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 console.log("Hello world!");
 // loading
 $(window).load(function() { // 整個頁面讀取完再將這三個 div 隱藏起來
-    $("#status").delay(1500).fadeOut(1500); //delay 延遲 2 秒才fadeOut
-    $("#preloader").delay(500).fadeOut(1500);
+    $("#status").delay(1250).fadeOut(1250); //delay 延遲 2 秒才fadeOut
+    $("#preloader").delay(250).fadeOut(1250);
 })
